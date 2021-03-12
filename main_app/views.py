6c03475bc2a1fe5dcce194cd3b6474dc971e4bee
Jon Apagueno <jonathan.apagueno@gmail.com>
2021-03-12 00:41:28 -0800

    adds login import to fix login problem after signup

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.contrib.auth.forms import UserCreationForm
-
+from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 
