94761c9d9129afb6930a34a99a18117d087657f4
Freddy Jimenez <freddy.junior@gmail.com>
2021-03-17 17:03:15 -0500

    updated error and added comments

--- main_app/models.py
+++ main_app/models.py
@@ -42,12 +42,11 @@ class Workout(models.Model):
 class Comment(models.Model):
     comment = models.TextField(max_length=120)
     date =  models.DateField(auto_now_add=True)
-
     workout = models.ForeignKey(Workout, on_delete=models.CASCADE)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.comment} on {self.date} by {self.author.username}"
-
+        
     class Meta:
         ordering = ['-date']
